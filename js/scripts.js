6bad4401e6ca686c5abc49ff4e4fabbc6815b4fb
Student <example@example.com>
2019-08-28 14:03:12 -0700

    Adds totals of both players and switches turns.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,15 @@
 var playerOne;
 var playerTwo;
 
+// Player Constructor
 function Player (player, score) {
   this.player = player,
   this.subtotal = [],
   this.subtotalSum = 0,
   this.score = score
 }
+
+// Player prototype
 Player.prototype.rollTotal = function() {
   for (var i = 0; i < this.subtotal.length; i++) {
     this.subtotalSum = this.subtotal[i]
@@ -16,6 +19,7 @@ Player.prototype.rollTotal = function() {
   return this.score
 }
 
+// Player v Player Start Button
 $(document).ready(function() {
   $("#twoPlayerStart").click(function(event) {
     event.preventDefault();
@@ -24,6 +28,7 @@ $(document).ready(function() {
     playerTwo = new Player ("player two", 0);
   });
 
+// Player One Roll Button
   $("#playerOneRoll").click(function(event) {
     var rand =  1 + Math.floor(Math.random() * 6);
     if (rand === 1) {
@@ -38,34 +43,39 @@ $(document).ready(function() {
       console.log(playerOne.subtotal);
      });
 
+// Player One Hold Button
   $("#playerOneHold").click(function(event) {
     $(".player1").hide();
     $(".player2").show();
     playerOne.score = playerOne.rollTotal();
-    console.log(playerOne);
+    console.log(playerOne.score);
     playerOne.subtotal = [];
     playerOne.subtotalSum = 0;
   });
 
+// Player Two Roll Button
   $("#playerTwoRoll").click(function(event) {
     var rand =  1 + Math.floor(Math.random() * 6);
        if (rand === 1) {
            playerTwoSubTotal = [];
            $(".player1").show();
            $(".player2").hide();
+           playerTwo.subtotal = [];
+           playerTwo.subtotalSum = 0;
           }
           else {
             playerTwo.subtotal.push(rand);
           }
+          console.log(playerTwo.subtotal)
      })
 
+// Player Two Hold Button
      $("#playerTwoHold").click(function(event) {
-
-
+       $(".player2").hide();
+       $(".player1").show();
+       playerTwo.score = playerTwo.rollTotal();
+       console.log(playerTwo.score);
+       playerTwo.subtotal = [];
+       playerTwo.subtotalSum = 0;
      });
-
-
-
-
-
 });
